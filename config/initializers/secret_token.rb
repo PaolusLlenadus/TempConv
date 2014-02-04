8e8abd3a413d34f9a04de33615174096e58cb2b3
YellowCake <paolusllenadus@gmail.com>
2014-02-04 07:32:05 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TempConv::Application.config.secret_key_base = '22f8f2b01b1cffb4122736e0177dd0f3d98795539f97a2e49dcbae17249296d7a7facb1446785dccc1f31092d10835e812d44d3d7464cd5d67f91a5a4386224a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TempConv::Application.config.session_store :cookie_store, key: '_TempConv_session'
